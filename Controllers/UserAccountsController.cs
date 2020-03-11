0308414d4d1a821ef5290bf71ba4ced12540f6ad
Nicolas Timm <nntimm1@gmail.com>
2020-03-11 11:32:34 -0500

    additional work on login and register routing to proper pages. also fixed link from registration to userAccount

--- Controllers/UserAccountsController.cs
+++ Controllers/UserAccountsController.cs
@@ -72,10 +72,11 @@ namespace Authentication.Controllers
                 //var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier)
                 var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
                 var user = _userManager.FindByIdAsync(userId).Result;
+                userAccount.UserName = user.Email;
                 userAccount.Wallet = new Wallet() { Balance = 0 };
                 userAccount.Wallet.Payment = new Payment() { CCNumber = 0 };
                 userAccount.Wallet.Transactions = new Transactions() { SentToWallet = false };
-                userAccount.UserName = user.Email;
+
 
                 _context.UserAccount.Add(userAccount);
        
@@ -171,10 +172,12 @@ namespace Authentication.Controllers
         {
             return _context.UserAccount.Any(e => e.UserId == id);
         }
-        public IActionResult UserHomePage(int? id)
+        public IActionResult UserHomePage()
         {
             var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if(userId == null)
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var userAccount = user.Email;
+            if (userAccount == null)
             {
                 return Create();
             }
@@ -184,7 +187,7 @@ namespace Authentication.Controllers
                 .Include(g => g.Group)
                 .Include(w => w.Wallet)
                 .Include(p => p.Wallet.Payment)
-                .Where(x => x.UserName == userId).FirstOrDefaultAsync();
+                .Where(x => x.UserName == userAccount).FirstOrDefaultAsync();
             if (User == null)
             {
                 return Create();
